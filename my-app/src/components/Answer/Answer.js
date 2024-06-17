15d0d753ab1fb3eab975e5440608d4e29e63534f
Dieter Roark <epictetus@kenkavn.com>
2024-06-17 05:56:49 -0400

    install https://create-react-app.dev/

--- my-app/src/components/Answer/Answer.js
+++ my-app/src/components/Answer/Answer.js
@@ -3,9 +3,8 @@ import React, { useState } from 'react';
 const Answer = ({text}) => {
     // Answer component logic and state management
     return (
-      <li className="answer-item">{text}</li>
+        <li className="answer-item">{text}</li>
     );
 };
 
 export default Answer;
-
